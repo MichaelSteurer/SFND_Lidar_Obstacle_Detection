59e2250aa2324f5395af3a4a1afab31f616dbe8c
Michael <msteurer@mailbox.org>
2021-01-10 20:01:24 +0000

    Implement euclideanCluster

--- src/quiz/cluster/cluster.cpp
+++ src/quiz/cluster/cluster.cpp
@@ -75,13 +75,46 @@ void render2DTree(Node* node, pcl::visualization::PCLVisualizer::Ptr& viewer, Bo
 
 }
 
+bool findInList(int point, std::vector<int> *points)
+{
+	return std::find(points->begin(), points->end(), point) != points->end();
+} 
+
+void proximity(const std::vector<std::vector<float>>& points, int index, KdTree* tree, float distanceTol, pcl::PointIndices::Ptr cluster, std::vector<int> *processedPointIndexes) 
+{
+	processedPointIndexes->push_back(index);
+	cluster->indices.push_back(index);
+
+	std::vector<float> point = points[index];
+	std::vector<int> nearbyPointIndexes = tree->search(point, distanceTol);
+	for(int i = 0; i < nearbyPointIndexes.size(); i++)
+	{
+		int nearbyPointIndex = nearbyPointIndexes[i];
+		if(!findInList(nearbyPointIndex, processedPointIndexes))
+		{
+			std::vector<float> nearbyPoint = points[nearbyPointIndex];
+			proximity(points, nearbyPointIndex, tree, distanceTol, cluster, processedPointIndexes);
+		}
+	}
+}
+
 std::vector<std::vector<int>> euclideanCluster(const std::vector<std::vector<float>>& points, KdTree* tree, float distanceTol)
 {
-
-	// TODO: Fill out this function to return list of indices for each cluster
-
 	std::vector<std::vector<int>> clusters;
- 
+
+	std::vector<int> processedPointIndexes;
+	for(int pointsIndex = 0; pointsIndex < points.size(); pointsIndex++) 
+	{
+		if (!findInList(pointsIndex, &processedPointIndexes))
+		{
+		    pcl::PointIndices::Ptr cluster(new pcl::PointIndices());
+
+			std::vector<float> point = points[pointsIndex];
+			proximity(points, pointsIndex, tree, distanceTol, cluster, &processedPointIndexes);
+			clusters.push_back(cluster->indices);
+		}
+	}
+
 	return clusters;
 
 }
