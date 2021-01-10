ce2058b64daa8c5a1d7a8efd98b144db1c93903a
Michael <msteurer@mailbox.org>
2021-01-10 16:02:15 +0000

    KDTree Search

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -70,10 +70,81 @@ struct KdTree
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
 		std::vector<int> ids;
+		iterateNodesRecursively(&root, target, distanceTol, 0, &ids);
 		return ids;
 	}
 
+	void iterateNodesRecursively(Node **node, std::vector<float> target, float distanceTol, int depth, std::vector<int> *foundIds)
+	{		
+		if (*node == NULL) // termination condition
+		{
+			// std::cout << "iterateNodesRecursively: Node is NULL" << std::endl;
+			return;
+		}
 
+		// std::cout << "iterateNodesRecursively: Node " << (*node)->point[0] << "/" << (*node)->point[1] << std::endl;
+		int pointIndex;
+		if (depth % 2) // 1, 3, 5, ... check y
+		{
+			pointIndex = 1;
+		}
+		else // 0, 2, 4, ... check x
+		{
+			pointIndex = 0;
+		}
+
+		float distanceLinePoint = (*node)->point[pointIndex] - target[pointIndex];
+		bool lineOutsideBox = abs(distanceLinePoint) > distanceTol;
+
+		if (lineOutsideBox)
+		{
+			if (distanceLinePoint) // point is on the right side if value is positive
+			{
+				iterateNodesRecursively(&((*node)->right), target, distanceTol, depth + 1, foundIds);
+			}
+			else // point is on the left side
+			{
+				iterateNodesRecursively(&((*node)->left), target, distanceTol, depth + 1, foundIds);
+			}
+		}
+		else // line is inside the current point's box
+		{
+			bool pointInsideRadius = isPointInsideRadius(target, (*node)->point, distanceTol);
+			if (pointInsideRadius)
+			{
+				// std::cout << "iterateNodesRecursively: Node is a match " << (*node)->id << std::endl;
+				(*foundIds).push_back((*node)->id);
+			}
+
+			iterateNodesRecursively(&((*node)->left), target, distanceTol, depth + 1, foundIds);
+			iterateNodesRecursively(&((*node)->right), target, distanceTol, depth + 1, foundIds);
+		}
+	}
+
+
+	bool isPointInsideRadius(std::vector<float> point, std::vector<float> boxCenter, float distanceTol)
+	{
+		// get lower and upper bounds of the square box
+		float boxMaxX = boxCenter[0] + distanceTol;
+		float boxMinX = boxCenter[0] - distanceTol;
+		float boxMaxY = boxCenter[1] + distanceTol;
+		float boxMinY = boxCenter[1] - distanceTol;
+
+		// quick pre-check wether the point is outside this square box
+		if (point[0] < boxMinX || point[0] > boxMaxX ||
+			point[1] < boxMinY || point[1] > boxMaxY)
+		{
+			return false; 
+		}
+
+		// point is within the square box but not necssarily within the radius
+
+		float deltaX = abs(point[0] - boxCenter[0]);
+		float deltaY = abs(point[1] - boxCenter[1]);
+		float distance = sqrt(pow(deltaX, 2) + pow(deltaY, 2));
+
+		return distance < distanceTol;
+	}
 };
 
 
